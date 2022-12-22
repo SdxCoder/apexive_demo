12e6f954a46e646f2a5f53276c562aed75171813
Saad Ahmed <saad.ahmed@leanscale.com>
2022-12-22 09:49:16 +0300

    add: firebase config and flutter create

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Installing dependencies..."
+flutter pub add firebase_core
+flutter pub add firebase_auth
+flutter pub add http
+flutter pub add build_runner
+flutter pub add chopper
+flutter pub add chopper_generator
+flutter pub add auto_route
+flutter pub add auto_route_generator
+flutter pub add injectable
+flutter pub add injectable_generator
+flutter pub add freezed
+flutter pub add freezed_annotation
+flutter pub add get_it
+flutter pub add flutter_bloc
+flutter pub add collection
+flutter pub add fpdart
